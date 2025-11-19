c1d49bea4ad8d74aee74652cf7992b7db002e941
chenulgamemaker <cdamvidu@gmail.com>
2025-11-19 19:28:00 +0200

    Add Game class and initialize on page load

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
 
+class Game {
+    constructor() {
+        this.economy = new Economy();
+        this.ui = new UI(this);
+    }
+}
+
+// Start the game when page loads
+window.addEventListener('load', () => {
+    new Game();
+});
